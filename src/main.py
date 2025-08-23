9864ea9583d6d219eee25e7c0107c698b2b8ac0e
Joao Marcelino <dev.marcelino@hotmail.com>
2025-08-23 17:31:37 -0300

    Implementa arquivo main para chamada das funções, adição dos arquivos .gitignore, requirements.txt e .env, essenciais para execução do projeto

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from src.database.models import Servidor
+from src.database.orm import ORM
+from dotenv import load_dotenv
+from services import get_servidores, save_servidores, save_verbas_indenizatorias, save_diarias
+from src.services import save_remuneracoes
+
+load_dotenv()
+orm = ORM()
+
+orm.generateBD()
+
+# Consulta os servidores públicos da API
+servidores = get_servidores()
+
+# Salva os servidores públicos no banco de dados
+save_servidores(servidores)
+#
+save_verbas_indenizatorias()
+save_diarias()
+save_remuneracoes()
+
+
+
+
+
