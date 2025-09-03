987c48092b6b10f226e6c99b14e406f747d27258
Joao Marcelino <dev.marcelino@hotmail.com>
2025-09-03 18:21:36 -0300

    Ajusta problema no import no main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 from src.database.models import Servidor
 from src.database.orm import ORM
 from dotenv import load_dotenv
-from services import get_servidores, save_servidores, save_verbas_indenizatorias, save_diarias
-from src.services import save_remuneracoes
+from src.services import get_servidores, save_servidores, save_verbas_indenizatorias, save_diarias, save_remuneracoes
 
 load_dotenv()
 orm = ORM()
 orm.generateBD()
 
 # Consulta os servidores públicos da API
-# servidores = get_servidores()
+servidores = get_servidores()
 
 # Salva os servidores públicos no banco de dados
-# save_servidores(servidores)
-# save_verbas_indenizatorias()
-# save_diarias()
+save_servidores(servidores)
+save_verbas_indenizatorias()
+save_diarias()
 save_remuneracoes()
 
 
