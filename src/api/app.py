dfdc536ca09173765a578f98ca86326f55bb64e8
Joao Marcelino <dev.marcelino@hotmail.com>
2025-09-05 16:04:15 -0300

    Ajusta os endpoints da API para receber searchParams

--- src/api/app.py
+++ src/api/app.py
@@ -1,30 +1,42 @@
+import datetime
 import json
 import os
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from sqlalchemy.orm.session import Session
-from sqlalchemy.sql.expression import select, desc
+from sqlalchemy.sql.expression import select, desc, extract
 from sqlalchemy.sql.functions import func
 
-from src.database.models import Servidor, Remuneracao, Diaria, VerbaIndenizatoria
+from src.database.models import Servidor, Remuneracao, Diaria, VerbaIndenizatoria, VerbaIndenizatoriaDetalhes
 
 from src.database.orm import ORM
+from datetime import date, datetime
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/salarios/total-liquido")
 def salarios_total_liquido():
-    with (ORM() as db):
-        session: Session = db.session
+    nome = request.args.get('nome', '')
+    maiorque = request.args.get('maiorque', 0.0)
+    maiorque = 0.0 if maiorque == '' else float(maiorque)
+    mes = int(request.args.get('mes', date.today().month))
+    ano = int(request.args.get('ano', date.today().year))
 
+    with ORM() as db:
+        session: Session = db.session
         stmt = select(
             Servidor.nome,
             Remuneracao.total_liquido,
             Remuneracao.data
         ).join(Remuneracao
-               ).group_by(Remuneracao.data
+               ).where(
+            Servidor.nome.like('%{}%'.format(nome)),
+            Remuneracao.total_liquido > maiorque,
+            extract("year", Remuneracao.data) == ano,
+            extract("month", Remuneracao.data) == mes
+                        ).group_by(Remuneracao.data
                         ).order_by(desc(Remuneracao.total_liquido)
                           )
         result = session.execute(stmt).mappings().all()
@@ -34,16 +46,24 @@ def salarios_total_liquido():
 
 @app.route("/diarias/valor-total")
 def diarias_valor_total():
+    nome = request.args.get('nome', '')
+    maiorque = request.args.get('maiorque', 0.0)
+    maiorque = 0.0 if maiorque == '' else float(maiorque)
+
     with ORM() as db:
         session: Session = db.session
-
+        soma_total = func.sum(Diaria.valor_total)
+        soma_diarias = func.sum(Diaria.numero_diarias)
         stmt = select(
             Servidor.nome,
-            func.sum(Diaria.valor_total).label("valor_total"),
-            func.sum(Diaria.numero_diarias).label("num_diarias")
+            soma_total.label("valor_total"),
+            soma_diarias.label("num_diarias")
         ).join(Diaria
-               ).group_by(Servidor.nome
-                          ).order_by(desc("valor_total"))
+               ).where(
+            Servidor.nome.like('%{}%'.format(nome)),
+        ).group_by(Servidor.nome
+                          ).having(soma_total > maiorque
+                                   ).order_by(desc("valor_total"))
 
         result = session.execute(stmt).mappings().all()
         result_dicts = [dict(row) for row in result]
@@ -52,39 +72,118 @@ def diarias_valor_total():
 
 @app.route("/indenizacoes/valor-total")
 def indenizacoes_valor_total():
-    with (ORM() as db):
+    nome = request.args.get('nome', '')
+    maiorque = request.args.get('maiorque', 0.0)
+    maiorque = 0.0 if maiorque == '' else float(maiorque)
+    with ORM() as db:
         session: Session = db.session
+        soma_total = func.sum(VerbaIndenizatoria.total_pago)
 
         stmt = select(
             Servidor.nome,
-            func.sum(VerbaIndenizatoria.total_pago).label("valor_total")
+            soma_total.label("valor_total")
         ).join(VerbaIndenizatoria
-               ).group_by(Servidor.nome
-                          ).order_by(desc("valor_total")
+               ).where(
+            Servidor.nome.like('%{}%'.format(nome))
+        ).group_by(Servidor.nome
+                          ).having(soma_total > maiorque
+        ).order_by(desc("valor_total")
                           )
         result = session.execute(stmt).mappings().all()
         result_dicts = [dict(row) for row in result]
 
     return jsonify(result_dicts)
 
-@app.route("/indenizacoes")
+@app.route("/indenizacoes/total-tempo")
 def indenizacoes_por_tempo():
-    data_inicio = request.args.get('data_inicio')
-    data_fim = request.args.get('data_fim')
+    data_inicio = request.args.get('dt_inicio', None)
+    data_fim= request.args.get('dt_fim', None)
+    maiorque = request.args.get('maiorque', 0.0)
+    maiorque = 0.0 if maiorque == '' else float(maiorque)
+
+    filtros = [
+
+    ]
+
+    if data_inicio:
+        data_inicio = datetime.strptime(data_inicio, "%Y-%m-%d")
+        filtros.append(VerbaIndenizatoria.data >= data_inicio)
+
+    if data_fim:
+        data_fim = datetime.strptime(data_fim, "%Y-%m-%d")
+        filtros.append(VerbaIndenizatoria.data <= data_fim)
+
+
 
     with (ORM() as db):
         session: Session = db.session
+        soma_total = func.sum(VerbaIndenizatoria.total_pago)
 
         stmt = select(
             VerbaIndenizatoria.data,
-            func.sum(VerbaIndenizatoria.total_pago).label("valor_total")
+            soma_total.label("valor_total")
+        ).where(
+            *filtros
         ).group_by(VerbaIndenizatoria.data
-                  ).order_by(VerbaIndenizatoria.data
+                  ).having( soma_total > maiorque
+        ).order_by(VerbaIndenizatoria.data
                             )
         result = session.execute(stmt).mappings().all()
         result_dicts = [dict(row) for row in result]
 
     return jsonify(result_dicts)
+
+@app.route("/indenizacoes")
+def indenizacoes():
+    nome = request.args.get('nome', '')
+    data = request.args.get('data')
+    maiorque = request.args.get('maiorque', 0.0)
+    maiorque = 0.0 if maiorque == '' else float(maiorque)
+    data = datetime.strptime(data, "%Y-%m") if data else None
+
+    filtros = [
+        Servidor.nome.like('%{}%'.format(nome)),
+        VerbaIndenizatoria.total_pago > maiorque,
+    ]
+
+    if data:
+        filtros.extend(
+           [
+               extract("year", VerbaIndenizatoria.data) == data.year,
+                extract("month", VerbaIndenizatoria.data) == data.month
+           ]
+        )
+
+
+
+
+
+    with (ORM() as db):
+        session: Session = db.session
+
+        stmt = select(
+            VerbaIndenizatoria.id,
+            VerbaIndenizatoria.data,
+            VerbaIndenizatoria.total_pago,
+            Servidor.nome,
+            VerbaIndenizatoriaDetalhes.classe.label("classe_servico"),
+            VerbaIndenizatoriaDetalhes.prestador,
+            VerbaIndenizatoriaDetalhes.data.label("data_servico"),
+            VerbaIndenizatoriaDetalhes.valor
+
+        ).join(
+            Servidor
+        ).join(
+            VerbaIndenizatoriaDetalhes
+        ).where(
+            *filtros
+        ).order_by(VerbaIndenizatoria.id
+        )
+        result = session.execute(stmt).mappings().all()
+        result_dicts = [dict(row) for row in result]
+
+    return jsonify(result_dicts)
+
 
 
 if __name__ == "__main__":
